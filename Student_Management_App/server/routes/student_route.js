b505c06c4cc763c04a669cbd81eab02e55dee133
nethma326 <nethmabaduge326@gmail.com>
2024-03-06 15:41:59 +0530

    practise routes

--- Student_Management_App/server/routes/student_route.js
+++ Student_Management_App/server/routes/student_route.js
@@ -4,83 +4,73 @@ let studentModel = require("../models/student_model");
 const router = express.Router();
 
 // add new student
-router.post("/addStudent", async (req, res) => {
+router.post("/addStudents", async (req, res) => {
     const { name, age, gender } = req.body;
 
-    const newStudent = new studentModel({
-        name,
-        age,
-        gender,
-    });
+    const newStudent = new studentModel({ name, age, gender });
 
     await newStudent
         .save()
         .then((result) => {
             res.status(200).json({
-                message: "Student details are saved successfully",
+                message: "Student details are save succuessfully!",
                 result,
             });
         })
         .catch((err) => {
-            res.status(400).json({
-                error: err,
-            });
+            res.status(400).json({ message: err });
         });
 });
 
 // get all students details
-router.get("/getStudent", async (req, res) => {
+router.get("/getStudents", async (req, res) => {
     await studentModel
         .find()
         .then((result) => {
             res.status(200).json({
-                message: "Student details are here",
+                message: "Students details are here",
                 result,
             });
         })
         .catch((err) => {
-            res.status(400).json({
-                error: err,
-            });
+            res.status(400).json({ eror: err });
         });
 });
 
 // update specific student detail
-router.put("/updateStudent/:id", async (req, res) => {
+router.put("/updateStudents/:id", async (req, res) => {
     const id = req.params.id;
     const { name, age, gender } = req.body;
 
-    const updateStudent = { name, age, gender };
+    const updatedStudent = { name, age, gender };
 
     await studentModel
-        .findByIdAndUpdate(id, updateStudent)
-        .then(() => {
+        .findByIdAndUpdate(id, updatedStudent)
+        .then((result) => {
             res.status(200).json({
-                message: "Student details is updated successfully",
+                message: "Student details are updated!",
+                result,
             });
         })
         .catch((err) => {
-            res.status(400).json({
-                error: err,
-            });
+            res.status(400).json({ error: err });
         });
 });
 
 // delete specific student detail
-router.delete("/deleteStudent/:id", async (req, res) => {
+router.delete("/deleteStudents/:id", async (req, res) => {
     const id = req.params.id;
 
     await studentModel
         .findByIdAndDelete(id)
-        .then(() => {
+        .then((result) => {
             res.status(200).json({
-                message: "Student detail is deleted successfully",
+                message: "Student details are deleted succesfully!",
+                result,
             });
         })
         .catch((err) => {
-            res.status(400).json({
-                error: err,
-            });
+            res.status(400).json({ error: err });
         });
 });
 
